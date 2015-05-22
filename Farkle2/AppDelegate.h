2a74ac8a5e76c971cce4d15a25aed910f93cb50c
Alex <alexsantorineos@gmail.com>
2015-05-22 07:18:32 -0500

    Initial Commit

---
+++ Farkle2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Farkle2
+//
+//  Created by Alex Santorineos on 5/22/15.
+//  Copyright (c) 2015 madApperz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
